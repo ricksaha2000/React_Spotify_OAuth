4184bc914ade6171431875d9ed81fc23bdcd0465
saha rick <jayitsaha@gmail.com>
2020-06-22 22:07:33 +0530

    DONE BACKEND

---
+++ spotify-frontend/spotify-backend/server.js
@@ -0,0 +1,46 @@
+let express = require('express')
+let request = require('request')
+let querystring = require('querystring')
+
+let app = express()
+
+let redirect_uri =
+  process.env.REDIRECT_URI ||
+  'http://localhost:8888/callback'
+
+app.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: 'e7b4933edab44bd08bfc5d82dd87a6eb',
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+})
+
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        'e7b4933edab44bd08bfc5d82dd87a6eb' + ':' + '08b8c7911de040188eb90f3ff287cb7c'
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
+
+let port = process.env.PORT || 8888
+console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
+app.listen(port)
